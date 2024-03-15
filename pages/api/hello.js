03a2f891ea8d07c034358ba572668fff8f4b4f7e
nithish <nithishreddygade@gmail.com>
2024-03-15 20:22:13 +0530

    news

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+
+  const requestBody = req.body
+  const studentName = requestBody.studentName;
+  const studentRollNumber = requestBody.studentRollNumber;
+  console.log("name ",studentName , " Roll Number ",studentRollNumber);
+  
+  
+  // Store in momngodb 
+
+
+  res.status(200).json({ message : true});
 }
